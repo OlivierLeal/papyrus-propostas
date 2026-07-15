2f5707fba7c5ecf64adbdad040ff568197b6eba0
Olivier Leal <olivierleal61@gmail.com>
2026-07-15 16:49:38 -0300

    feat: adiciona layout do menu e atualiza design do sistema

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -3,7 +3,7 @@ module Authentication
 
   included do
     before_action :require_authentication
-    helper_method :authenticated?
+    helper_method :authenticated?, :current_user
   end
 
   class_methods do
@@ -19,6 +19,10 @@ module Authentication
 
     def require_authentication
       resume_session || request_authentication
+    end
+
+    def current_user
+      Current.user
     end
 
     def resume_session
