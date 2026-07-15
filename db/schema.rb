8f117a9a2c3bd1a8f7927b26d28d5483559f6fbb
Olivier Leal <olivierleal61@gmail.com>
2026-07-15 15:56:18 -0300

    feat: adiciona migration que ativa postgis no banco

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_15_183136) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_15_185455) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+  enable_extension "postgis"
 
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", null: false
